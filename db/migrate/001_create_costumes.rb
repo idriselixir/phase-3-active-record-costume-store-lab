ec2e968b89a5e9afa58b64e7c7227e5ad82c9928
Idris Noor <idreesnoor150@gmail.com>
2023-05-31 21:25:27 +0300

    done

---
+++ db/migrate/001_create_costumes.rb
@@ -0,0 +1,14 @@
+# db/migrate/001_create_costumes.rb
+
+class CreateCostumes < ActiveRecord::Migration[6.1]
+    def change
+      create_table :costumes do |t|
+        t.string :name
+        t.float :price
+        t.string :size
+        t.string :image_url
+        t.timestamps
+      end
+    end
+  end
+  
